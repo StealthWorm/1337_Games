1ddd21f4e7a476dbf76b7520c3f58bd084d414d5
StealthWorm <thierrypitela@hotmail.com>
2024-01-29 18:41:28 -0300

    fix: fix responsiveness

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,13 +10,13 @@ export function Home() {
       <section id="home" className='flex h-screen snap-center'>
         <Hero />
       </section>
-      <section id="games" className='flex h-screen snap-start pt-[4rem] md:pt-[5rem]'>
+      <section id="games" className='flex h-screen snap-start pt-[4rem] md:pt-[5rem] lg:pt-[8rem]'>
         <Games />
       </section>
-      <section id="about" className='flex h-screen snap-start pt-[4rem] md:pt-[5rem]'>
+      <section id="about" className='flex h-screen snap-start pt-[4rem] md:pt-[5rem] lg:pt-[8rem]'>
         <About />
       </section>
-      <section id="contact" className='flex h-screen snap-start pt-[4rem] md:pt-[5rem]'>
+      <section id="contact" className='flex h-screen snap-start pt-[4rem] md:pt-[5rem] lg:pt-[8rem]'>
         <Contact />
       </section>
     </main>
