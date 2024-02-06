336342f5a0f1ce468daea8551f74ff9a5e1fea19
StealthWorm <thierrypitela@hotmail.com>
2024-02-06 17:34:34 -0300

    feat: fixed contac and about pages with inclusion of form for email sending

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,20 +1,35 @@
+import { Money } from "@phosphor-icons/react"
 import { useTranslation } from "react-i18next"
 
 export function About() {
   const { t } = useTranslation()
 
   return (
-    <div className="flex flex-col h-full w-full backdrop-blur-lg bg-green-primary/80  p-8 gap-4 overflow-hidden">
-      <h3 className="flex uppercase text-3xl md:text-5xl text-green-primary-light justify-center font-jomhuria tracking-wider">
+    <div className="relative flex flex-col h-full w-full bg-green-primary/70 backdrop-blur-lg gap-4 overflow-hidden items-center justify-start">
+      <h3 className="flex uppercase text-3xl md:p-8 md:text-5xl text-green-primary-light justify-center font-jomhuria tracking-wider">
         {t('header:app.about')}
       </h3>
-
-      <p>
-        Um projeto Indie em desenvolvimento, a 1337 Games trabalha para resgatar a emoção dos clássicos JRPGs.
-      </p>
-      <strong>
-        Este projeto sendo desenvolvido por apenas uma pessoa
-      </strong>
+      <div className="flex flex-col justify-between w-full h-full">
+        <div className="flex flex-col px-6">
+          <p>
+            Um projeto Indie em desenvolvimento, a 1337 Games trabalha para resgatar a emoção dos clássicos JRPGs.
+          </p>
+          <strong>
+            Este projeto sendo desenvolvido por apenas uma pessoa
+          </strong>
+        </div>
+        <div className='flex w-full flex-col md:flex-row gap-6 font-jomhuria bg-green-primary-dark py-8 px-auto px-4 items-center justify-between md:justify-center border-t border-b border-warning'>
+          <span className="tracking-wide text-2xl md:text-4xl text-center w-full md:w-[60%]">
+            {t('footer:app.donation.description')}
+          </span>
+          <a href="https://nubank.com.br/pagar/3wy6e/aJdlSZf8Mh" target="_blank" className='flex w-full md:w-[40%] hover:animate-none animate-pulse gap-6 justify-center items-center p-4 text-green-secondary-dark rounded-md bg-warning hover:brightness-150 hover:shadow-custom transition-all duration-200'>
+            <strong className='flex uppercase font-jomhuria tracking-widest text-xl md:text-4xl pt-2'>
+              {t('footer:app.donation.title')}
+            </strong>
+            <Money size={40} weight='bold' />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,30 +1,101 @@
-import { Money } from "@phosphor-icons/react"
+import { Envelope, MapPin, Phone } from "@phosphor-icons/react"
 import { useTranslation } from "react-i18next"
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const ClaimContactFormSchema = z.object({
+  name: z.string(),
+  email: z.string().email("Informe um e-mail válido"),
+  subject: z.string().min(3, { message: "O assunto precisa ter pelo menos 10 caracteres" }),
+  message: z.string().min(10, { message: "O corpo do email precisa ter pelo menos 10 caracteres" })
+})
+
+type ClaimContactFormData = z.infer<typeof ClaimContactFormSchema>
 
 export function Contact() {
   const { t } = useTranslation()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimContactFormData>({
+    resolver: zodResolver(ClaimContactFormSchema),
+  })
+
+  function handleSendMail(data: ClaimContactFormData) {
+    window.location.href = `mailto:leandro.nunes19xl@gmail.com?subject=${data.subject}&body=${encodeURIComponent(`Hi, my name is ${data.name} and my email is ${data.email}.\n${data.message}`)}`;
+  }
 
   return (
-    <div className="flex flex-col h-full w-full bg-green-primary/90 backdrop-blur-lg bg-green-primary gap-4">
-      <h3 className="p-8 flex uppercase text-3xl md:text-5xl text-green-primary-light justify-center font-jomhuria tracking-wider">
+    <div className="relative flex flex-col h-full w-full bg-green-primary/70 backdrop-blur-lg md:p-8 gap-4 overflow-hidden items-center justify-start">
+      <h3 className="flex uppercase text-3xl md:text-5xl text-green-primary-light justify-center font-jomhuria tracking-wider">
         {t('header:app.contact')}
       </h3>
-      <div className="flex flex-col flex-1 w-full">
-        <div className="p-8 h-full">
-          conteudo de contato
+
+      <div className="flex flex-col md:w-max mx-4 w-full space-y-4">
+        <div className='flex md:flex-row flex-col items-center justify-between mx-4 gap-6'>
+          <div className='flex items-center gap-2'>
+            <Phone size={30} className='text-emerald-500 animate-pulse' />
+            <p className='text-md w-full'>+55 (81) 985632622</p>
+          </div>
+          <div className='flex items-center gap-2'>
+            <Envelope size={30} className='text-emerald-500 animate-pulse' />
+            <p className='text-md w-full'>leandro.nunes19xl@gmail.com</p>
+          </div>
+          <div className='flex items-center gap-2'>
+            <MapPin size={30} className='text-emerald-500 animate-pulse' />
+            <p className='text-md w-full'>Recife, Pernambuco, Brasil </p>
+          </div>
         </div>
 
-        <div className='flex w-full flex-col md:flex-row gap-6 font-jomhuria bg-green-primary-dark py-10 px-auto px-4 items-center justify-between md:justify-center border-t border-b border-warning'>
-          <span className="tracking-wide text-2xl md:text-4xl text-center w-full md:w-[60%]">
-            {t('footer:app.donation.description')}
-          </span>
-          <a href="https://nubank.com.br/pagar/3wy6e/aJdlSZf8Mh" target="_blank" className='flex w-full md:w-[40%] hover:animate-none animate-pulse gap-6 justify-center items-center p-4 text-green-secondary-dark rounded-md bg-warning hover:brightness-150 hover:shadow-custom transition-all duration-200'>
-            <strong className='flex uppercase font-jomhuria tracking-widest text-xl md:text-4xl pt-2'>
-              {t('footer:app.donation.title')}
-            </strong>
-            <Money size={40} weight='bold' />
-          </a>
-        </div>
+        <form onSubmit={handleSubmit(handleSendMail)} autoComplete="off" className='flex flex-col gap-6 md:w-fit mx-4 md:mx-auto'>
+          <div className='flex flex-col md:flex-row gap-4'>
+            <div className='flex flex-col md:flex-row'>
+              <input
+                id="name"
+                {...register('name')}
+                placeholder='Type your Name'
+                type='text'
+                className='contactInput'
+              />
+            </div>
+            <div className='flex flex-col'>
+              <input
+                id="email"
+                {...register('email')}
+                placeholder='Type your Email'
+                type='email'
+                className='contactInput'
+              />
+              {errors.email && <p className="errorMessage">{errors.email.message}</p>}
+            </div>
+          </div>
+
+          <div className='flex flex-col h-10'>
+            <input
+              id="subject"
+              {...register('subject')}
+              placeholder='Email subject'
+              type='text' 
+              className='contactInput'
+            />
+            {errors.subject && <p className="errorMessage">{errors.subject.message}</p>}
+          </div>
+          <div className='flex flex-col'>
+            <textarea
+              id="message"
+              {...register('message')}
+              placeholder='Type your Message'
+              className='contactInput'
+            />
+            {errors.message && <p className="errorMessage">{errors.message.message}</p>}
+          </div>
+
+          <button className='bg-emerald-500 py-4 px-10 rounded-md text-black font-bold text-lg' type='submit'>
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   )
